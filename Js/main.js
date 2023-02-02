3712e40140104fecf8a4a25a24d54273688477b7
Gustavo Marialva <gmarialva@gmail.com>
2023-02-02 09:49:31 -0300

    adiciona forEach para todos os elementos do robo

--- Js/main.js
+++ Js/main.js
@@ -1,14 +1,42 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const braco = document.querySelector("#braco");
+const somar = document.querySelector("#somar");
 
-robotron.addEventListener("click", () => {
-  console.log("cliquei no robo");
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.textContent);
+  });
 });
+
+// somar.addEventListener("click", () => {
+//   manipulaDados("somar");
+// });
+
+// subtrair.addEventListener("click", () => {
+//   manipulaDados("subtrair");
+// });
+
+function manipulaDados(operacao) {
+  if (operacao === "-") {
+    braco.value = parseInt(braco.value) - 1;
+  } else {
+    braco.value = parseInt(braco.value) + 1;
+  }
+}
+
+// const robotron = document.querySelector("#robotron");
+
+// robotron.addEventListener("click", () => {
+//   console.log("cliquei no robo");
+// });
 
 // Você pode usar function ou () => arrow function
 
-function dizOi(nome) {
-  console.log(nome);
-  console.log("Bora pro jogo!");
-}
+// function dizOi(nome) {
+//   console.log(nome);
+//   console.log("Bora pro jogo!");
+// }
 
-dizOi("Gustavo");
+// dizOi("Gustavo");
